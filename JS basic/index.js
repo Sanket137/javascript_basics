48ded22e8dea2e6f486a1453db6a45e7c3d7f8e2
Sanket Shende <67966316+Sanket137@users.noreply.github.com>
2022-07-11 12:25:12 +0530

    Basic JS practice
    Created a basic webpage using bootstrap v5.13

---
+++ JS basic/index.js
@@ -0,0 +1,51 @@
+// Array - variable that holds multiple values
+
+/* multi line 
+arrays */
+
+const toDoList = [ 
+    { 
+        id: 01,
+        task: 'learn basics of Html',
+        status: true
+    },
+
+    { 
+        id: 02,
+        task: 'learn basics of CSS',
+        status: true
+    },
+
+    { 
+        id: 03,
+        task: 'learn basics of javascript',
+        status: false
+    }
+
+
+
+
+ ];
+
+
+ toDoList.push(
+    {
+        id: 04,
+        task: 'learn Bootstrap',
+        status: false
+    },
+    {
+        id: 05,
+        task: 'learn React',
+        status: true
+    }
+)
+
+
+// for loop
+
+
+for (let i= 50 ; i >= -5 ; --i){
+    console.log(`this value ${i}`);
+}
+
